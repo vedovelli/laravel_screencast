b4cbea415646e1165fb8145b4a61da2bc467ccb0
Vedovelli <vedovelli@gmail.com>
2013-01-24 09:31:33 -0500

    Mais comentarios adicionados ao arquivo routes.php

--- application/routes.php
+++ application/routes.php
@@ -1,38 +1,46 @@
 <?php
 
 /**
-* A rota toor da app mostra a action index do controller home
+* Este é o arquivo que mapeia as URLs para as ações da sua aplicação.
+* É a porta de entrada da app. O Laravel lhe permite utilizar as rotas
+* de duas formas: 1) utilizando closures (métodos anônimos) e tratando
+* do comportamento da app diretamente neste arquivo (recomenado para
+* apps pequenas e testes) e 2) mapeando para ações contidas nos controllers
+* (método considerado best practice). De qualquer forma, Laravel lhe dá
+* a flexibilidade de utilizar ambos na mesma app. Fica a seu critério.
+* Eu sempre utilizo Controllers, pois valorizo bastante a separação de 
+* responsabilidades.
 */
-Route::get('/', function()
-{
+
+
+/**
+* A rota root da app redirectiona para action index do controller home
+*/
+Route::get('/', function(){
 	return Redirect::to('home');
 });
 
-Event::listen('404', function()
-{
+Event::listen('404', function(){
 	return Response::error('404');
 });
 
-Event::listen('500', function()
-{
+Event::listen('500', function(){
 	return Response::error('500');
 });
 
-Route::filter('before', function()
-{
-});
+// Route::filter('before', function()
+// {
+// });
 
-Route::filter('after', function($response)
-{
-});
+// Route::filter('after', function($response)
+// {
+// });
 
-Route::filter('csrf', function()
-{
+Route::filter('csrf', function(){
 	if (Request::forged()) return Response::error('500');
 });
 
-Route::filter('auth', function()
-{
+Route::filter('auth', function(){
 	/**
 	* Caso não haja usuario logado. Outra forma de verificar seria !Auth::check()
 	*/
@@ -45,7 +53,11 @@ Route::filter('auth', function()
 });
 
 /**
-* Cria automaticamente as rotas para actions dos controllers
+* Cria automaticamente as rotas para actions dos controllers.
+* Todos os métodos dos controllers abaixo que contém o prefixo
+* action_ responderão automaticamente quando a URL for acessada.
+* Ex. function action_lista() em Profile_Controller será executado
+* quando a URL http://seu_host/profile/lista for acessada.
 */
 Route::controller('home');
 Route::controller('profile');
